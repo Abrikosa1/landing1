680f866e0b7cc6b2d842572bc025e73673ab2608
unknown <world4den@mail.ru>
2020-01-28 04:41:15 +0600

    slider v0.9

--- js/main.js
+++ js/main.js
@@ -1,8 +1,56 @@
 // new WOW().init();
+/* var galleryThumbs = new Swiper('.slider-avatars', {
+    spaceBetween: 0,
+    slidesPerView: 5,
+    loop: true,
+    freeMode: true,
+    centeredSlides: true,
+    loopedSlides: 5, //looped slides should be the same
+    watchSlidesVisibility: true,
+    watchSlidesProgress: true, 
 
-var mySwiper = new Swiper('.swiper-container', {
-    spaceBetween: 30,
-    speed: 400,
-    spaceBetween: 100,
-    clickable: true
-});
+  }); */
+
+/* var mySwiper = new Swiper('.slider-text', {
+    spaceBetween: 10,
+      loop:true,
+      loopedSlides: 5, //looped slides should be the same
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+      thumbs: {
+        swiper: galleryThumbs,
+      },
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+        dynamicBullets: true,
+        renderBullet: function (index, className) {
+            return '<img src="images/clients/avatar/'+(index + 1)+'.png" class="' + className + '">' + (index + 1) + '</img>';
+        },
+        
+      },
+     
+  });*/
+
+  var mySwiper = new Swiper('.slider-text', {
+    spaceBetween: 10,
+      loop:true,
+      loopedSlides: 5, //looped slides should be the same
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+        dynamicBullets: true,
+        renderBullet: function (index, className) {
+            return '<img src="images/clients/avatar/'+(index + 1)+'.png" class="' + className + '">' + (index + 1) + '</img>';
+        },
+        
+      },
+    
+  });
+
