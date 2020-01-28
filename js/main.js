74444a1af374aeb4ec4fb0d34276c24c3cca30fc
unknown <world4den@mail.ru>
2020-01-28 18:21:10 +0600

    slider v0.95

--- js/main.js
+++ js/main.js
@@ -34,23 +34,32 @@
      
   });*/
 
-  var mySwiper = new Swiper('.slider-text', {
-    spaceBetween: 10,
-      loop:true,
-      loopedSlides: 5, //looped slides should be the same
-      navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-      },
-      pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
-        dynamicBullets: true,
-        renderBullet: function (index, className) {
-            return '<img src="images/clients/avatar/'+(index + 1)+'.png" class="' + className + '">' + (index + 1) + '</img>';
-        },
-        
-      },
-    
-  });
+var mySwiper = new Swiper('.slider-text', {
+  effect: 'coverflow',
+  centeredSlides: true,
+  slidesPerView: 'auto',
+  coverflowEffect: {
+    rotate: 50,
+    stretch: 0,
+    depth: 100,
+    modifier: 1,
+    slideShadows: true,
+  },
+  spaceBetween: 10,
+  loop: true,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+    dynamicBullets: 5,
+    hideOnClick: false,
+    renderBullet: function (index, className) {
+      return '<img src="images/clients/avatar/' + (index + 1) + '.png" class="' + className + '">' + (index + 1) + '</img>';
+    },
 
+  },
+
+});
