ff614be3123f74e79f5d0dfeddcd826b23ad9a5c
unknown <world4den@mail.ru>
2020-01-31 20:21:30 +0600

    anchors and some animation

--- js/main.js
+++ js/main.js
@@ -1,38 +1,4 @@
-// new WOW().init();
-/* var galleryThumbs = new Swiper('.slider-avatars', {
-    spaceBetween: 0,
-    slidesPerView: 5,
-    loop: true,
-    freeMode: true,
-    centeredSlides: true,
-    loopedSlides: 5, //looped slides should be the same
-    watchSlidesVisibility: true,
-    watchSlidesProgress: true, 
-
-  }); */
-
-/* var mySwiper = new Swiper('.slider-text', {
-    spaceBetween: 10,
-      loop:true,
-      loopedSlides: 5, //looped slides should be the same
-      navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-      },
-      thumbs: {
-        swiper: galleryThumbs,
-      },
-      pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
-        dynamicBullets: true,
-        renderBullet: function (index, className) {
-            return '<img src="images/clients/avatar/'+(index + 1)+'.png" class="' + className + '">' + (index + 1) + '</img>';
-        },
-        
-      },
-     
-  });*/
+new WOW().init();
 
 var mySwiper = new Swiper('.slider-text', {
   effect: 'coverflow',
@@ -60,34 +26,4 @@ var mySwiper = new Swiper('.slider-text', {
     },
 
   }, 
- 
-/*   breakpoints: {
-    // when window width is >= 320px
-    735: {
-      pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
-        dynamicBullets: 5,
-        hideOnClick: false,
-        renderBullet: function (index, className) {
-          return '<img src="images/clients/avatar/' + (index + 1) + '.png" class="' + className + '">' + (index + 1) + '</img>';
-        },
-
-      }
-    },
-    320: {
-      pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
-        dynamicBullets: 1,
-        dynamicMainBullets: 1,
-        hideOnClick: false,
-        renderBullet: function (index, className) {
-          return '<img src="images/clients/avatar/' + (index + 1) + '.png" class="' + className + '">' + (index + 1) + '</img>';
-        },
-
-      }
-    },
-  } */
-
 });
